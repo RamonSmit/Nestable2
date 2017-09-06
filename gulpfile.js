b0dfa9bacf464c6341c9f1431c03c15042118192
Marek Skiba <skibamarek@gmail.com>
2017-09-06 22:01:13 +0200

    added SASS version

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,32 @@ const uglify = require("gulp-uglify");
 const cleanCss = require("gulp-clean-css");
 const eslint = require("gulp-eslint");
 const rename = require("gulp-rename");
+const sass = require('gulp-sass');
 
 const file = 'jquery.nestable';
 
-gulp.task('default', function () {
+// compress js
+gulp.task('js', function () {
     gulp.src(file + '.js')
         .pipe(uglify())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('dist/'));
+});
 
+// compile SASS to CSS
+gulp.task('sass', function () {
+    return gulp.src(file + '.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('.'));
+});
+
+// compress css
+gulp.task('css', ['sass'], function () {
     gulp.src(file + '.css')
         .pipe(cleanCss())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('dist/'));
 });
-
 
 gulp.task('test', function () {
     return gulp.src([file + '.js'])
@@ -25,3 +36,6 @@ gulp.task('test', function () {
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
 });
+
+// build assets
+gulp.task('default', ['js', 'css']);
